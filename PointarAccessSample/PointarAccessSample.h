f5ffb9248d087c862bce0215bca640c3830b5d01
sak52jp <charan.49jp@gmail.com>
2020-10-18 01:12:33 +0900

    DLLサンプル側の余分な定義を削除した。

--- PointarAccessSample/PointarAccessSample.h
+++ PointarAccessSample/PointarAccessSample.h
@@ -5,5 +5,5 @@
 #define POINTARACCESSSAMPLE_API __declspec(dllimport)
 #endif
 
-extern "C" POINTARACCESSSAMPLE_API void __cdecl ConvertContents(int size, int* data);
+extern "C" POINTARACCESSSAMPLE_API void ConvertContents(int size, int* data);
 
